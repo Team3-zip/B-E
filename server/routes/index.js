344054d84252efb94f32732caddd0346663a0295
kwak9898 <80763040+kwak9898@users.noreply.github.com>
2021-12-30 00:12:37 +0900

    찜하기 라우터 추가
    찜하기 라우터 추가

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,12 +4,12 @@ const mainRouter = require('./main');
 const detailRouter = require('./detail');
 const userRouter = require('./user');
 const commentRouter = require('./comment');
+const likeRouter = require('./like');
 
 router.use('/main', mainRouter);
 router.use('/', detailRouter);
 router.use('/users', userRouter);
 router.use('/comments', commentRouter);
-
-
+router.use('/:likeId', likeRouter);
 
 module.exports = router;
