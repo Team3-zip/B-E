2315f5b6abd42f45b1b824c4d5b5cb892e854c94
hana <aswjdgksk@gmail.con>
2021-12-29 15:33:23 +0900

    comment 완료, 미들웨어 안붙인상테

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,10 +4,11 @@ const router = express.Router();
 
 const mainRouter = require('./main');
 const detailRouter = require('./detail');
+const commentRouter = require('./comment');
 
 router.use('/main', mainRouter);
 router.use('/', detailRouter);
-
+router.use('/comments', commentRouter);
 
 
 module.exports = router;
