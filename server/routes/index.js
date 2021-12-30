fa1a8aae5cf918df0eefdbbb5bde6a9d5b6170da
kwak9898 <80763040+kwak9898@users.noreply.github.com>
2021-12-30 11:07:28 +0900

    민영 및 공영 전체 리스트 수정
    민영 및 공영 전체 리스트 수정

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,15 +4,14 @@ const mainRouter = require('./main');
 const detailRouter = require('./detail');
 const userRouter = require('./user');
 const commentRouter = require('./comment');
-const privateListRouter = require('./privateNotice')
-const publicListRouter = require('./publicNotice')
+const listRouter = require('./list');
 const likeRouter = require('./like');
 
 router.use('/main', mainRouter);
 router.use('/', detailRouter);
 router.use('/users', userRouter);
 router.use('/comments', commentRouter);
-router.use('/list', privateListRouter, publicListRouter)
+router.use('/', listRouter);
 router.use('/:likeId', likeRouter);
 
 module.exports = router;
--- server/routes/privateNotice.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const privateControll = require('../controller/privateNotice')
-const router = express.Router()
-
-router.get('/private', privateControll.getPrivateNotice)
-router.get('/private2', privateControll.getPrivateNotice2)
-
-module.exports = router
