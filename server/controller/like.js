c5466bcd8415fdfc4e21219cf8af2e181bb20425
kwak9898 <80763040+kwak9898@users.noreply.github.com>
2021-12-31 22:28:50 +0900

    찜하기 수정
    찜하기 수정

--- server/controller/like.js
+++ server/controller/like.js
@@ -1,5 +1,6 @@
 const Likes = require('../models/Like')
 const Private = require('../models/PrivateApt')
+const Public = require('../models/PubNotice')
 const { Op } = require('sequelize')
 
 const createLike = async (req, res) => {
@@ -8,21 +9,25 @@ const createLike = async (req, res) => {
     // const { userKey } = res.locals.user
     try {
         const existLike = await Likes.findOne({ where: { fk_userKey: userKey } })
-        const existAptNo = await Private.findOne({ where: { pblancNo: aptNo } })
+        const existPrivatAptNo = await Private.findOne({ where: { pblancNo: aptNo } })
+        const existPublicAptNo = await Public.findOne({ where: { panId: aptNo } })
 
         if (!existLike) {
-            if (existAptNo) {
+            if (existPrivatAptNo) {
                 await Likes.create({ fk_userKey: userKey, fk_pblancNo: aptNo })
                 res.send((result = { data: true }))
-            } else { // 공영일거다
+            } else if (existPublicAptNo) { // 공영일거다
                 await Likes.create({ fk_userKey: userKey, panId: aptNo })
                 res.send((result = { data: true }))
             }
         } else {
-            await Likes.destroy({
-                where: { fk_userKey: userKey, [Op.or]: [{ fk_pblancNo: aptNo }, { panId: aptNo }] }
-            })
-            res.send((result = { data: false }))
+            if (existPrivatAptNo) {
+                await Likes.destroy({ where: { fk_userKey: userKey } }, { fk_pblancNo: aptNo })
+                res.send((result = { data: false }))
+            } else if (existPublicAptNo) {
+                await Likes.destroy({ where: { fk_userKey: userKey } }, { panId: aptNo })
+                res.send((result = { data: false }))
+            }
         }
     } catch (error) {
         console.log('-------------------------------')
