99175cb84438d235128d41e9ba7ab1ed0b0fa7ec
kwak9898 <80763040+kwak9898@users.noreply.github.com>
2021-12-31 23:06:34 +0900

    좋아요 수정 완료
    좋아요 수정 완료

--- server/controller/like.js
+++ server/controller/like.js
@@ -8,28 +8,29 @@ const createLike = async (req, res) => {
     const { userKey } = req.body
     // const { userKey } = res.locals.user
     try {
-        const existLike = await Likes.findOne({ where: { fk_userKey: userKey } })
-        const existPrivatAptNo = await Private.findOne({ 
-            attributes :['pblancNo'],
-            where : {pblancNo : aptNo},
-            raw : true
-         })
-        
+        const existLike = await Likes.findOne({
+            where: { fk_userKey: userKey, [Op.or]: { fk_pblancNo: aptNo, panId: aptNo } }
+        })
+        const existPrivatAptNo = await Private.findOne({
+            attributes: ['pblancNo'],   // 어떤 값을 찾을지
+            where: { pblancNo: aptNo }, // 조건
+            raw: true   // 부가적인 거 없고 데이터 값만 나오게
+        })
 
         if (!existLike) {
             if (existPrivatAptNo) {
                 await Likes.create({ fk_userKey: userKey, fk_pblancNo: aptNo })
                 res.send((result = { data: true }))
-            } else { // 공영일거다
+            } else {
                 await Likes.create({ fk_userKey: userKey, panId: aptNo })
                 res.send((result = { data: true }))
             }
         } else {
             if (existPrivatAptNo) {
-                await Likes.destroy({ where: { fk_userKey: userKey } }, { fk_pblancNo: aptNo })
+                await Likes.destroy({ where: { fk_userKey: userKey, fk_pblancNo: aptNo } })
                 res.send((result = { data: false }))
-            } else  {
-                await Likes.destroy({ where: { fk_userKey: userKey } }, { panId: aptNo })
+            } else {
+                await Likes.destroy({ where: { fk_userKey: userKey, panId: aptNo } })
                 res.send((result = { data: false }))
             }
         }
