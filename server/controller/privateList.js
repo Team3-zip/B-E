0607148d226f584e73ef2fcfd44535a51df52bdc
kwak9898 <80763040+kwak9898@users.noreply.github.com>
2022-01-04 15:35:46 +0900

    민영 및 공영 전체리스트 수정
    민영 및 공영 전체리스트 수정

--- server/controller/privateList.js
+++ server/controller/privateList.js
@@ -1,45 +1,47 @@
 const Private = require('../models/PrivateApt')
-const {Op} = require('sequelize');
-const {like, or} = Op
+const { Op } = require('sequelize');
+const { like, or } = Op
 
 
 const getPrivateNotice = async (req, res) => {
     const sido = req.query.sido;
-    const spell1 = sido.substring(0,1);
-    const spell2=sido.substring(1,2);
+    const spell1 = sido.substring(0, 1);
+    const spell2 = sido.substring(1, 2);
     console.log(spell1);
-    let priNotice='';
+    let priNotice = '';
     try {
-        if(spell2 ==='상'){
-             priNotice = await Private.findAll({
-                order: [["recruitDate", "ASC"]],
-                where :{
-                    [or]:[
-                        {sido : '경남'},
-                        {sido : '경북'}
-                    ]
-                },
-                raw:true
-             })
-        }else{
-            priNotice = await Private.findAll({
-                order: [["recruitDate", "ASC"]],
-                where: {
-                    [or]: [
-                        {
-                            sido: {
-                                [like]: `%${spell1}%`
-                            },
-                            // sido:{
-                            //     [like]:`%${spell2}%`
-                            // }
-                        }
-                    ]
-                },
-                raw:true
-            })
+        if (spell2 === '상') {
+            priNotice = await Private.query('SELECT sido, COUNT(*) FROM privateapt WHERE sido = sido OR sido = 경북 OR sido = 경남 ORDER BY recruitDate (ASC)')
+            //  priNotice = await Private.findAll({
+            //     order: [["recruitDate", "ASC"]],
+            //     where :{
+            //         [or]:[
+            //             {sido : '경남'},
+            //             {sido : '경북'}
+            //         ]
+            //     },
+            //     raw:true
+            //  })
+        } else {
+            priNotice = await Private.query('SELECT sido, COUNT(*) FROM privateapt WHERE sido = sido OR sido LIKE `${spel1}` OR sido Like `${spel2}` ORDER BY recruitDate (ASC)')
+            // priNotice = await Private.findAll({
+            //     order: [["recruitDate", "ASC"]],
+            //     where: {
+            //         [or]: [
+            //             {
+            //                 sido: {
+            //                     [like]: `%${spell1}%`
+            //                 },
+            //                 // sido:{
+            //                 //     [like]:`%${spell2}%`
+            //                 // }
+            //             }
+            //         ]
+            //     },
+            //     raw:true
+            // })
         }
-        //console.log(priNotice)
+        // console.log(priNotice)
         res.send({ result: priNotice })
     } catch (error) {
         res.send({ error })
@@ -59,5 +61,5 @@ const getPrivateNotice = async (req, res) => {
 
 module.exports = {
     getPrivateNotice,
-   // getPrivateNotice2
+    // getPrivateNotice2
 }
--- server/controller/publicList.js
+++ server/controller/publicList.js
@@ -1,31 +1,32 @@
 const Public = require('../models/PubNotice');
-const {Op} = require('sequelize');
-const {like, or} = Op
+const { Op } = require('sequelize');
+const { like, or } = Op
 
 //쿼리파라미터 없으면 전체리스트 
 const getPublicNotice = async (req, res) => {
     const sidoName = req.query.sidoName;
     console.log(sidoName)
-    const spell1 = sidoName.substring(0,1);
-    const spell2=sidoName.substring(1,2);
+    const spell1 = sidoName.substring(0, 1);
+    const spell2 = sidoName.substring(1, 2);
     console.log(spell1, spell2);
     try {
-        const pubNotice = await Public.findAll({
-            order: [["panUploadDate", "ASC"]],
-            where: {
-                [or]: [
-                    {
-                        sidoName: {
-                            [like]: `%${spell1}%`
-                        },
-                        sidoName:{
-                            [like]:`%${spell2}%`
-                        }
-                    }
-                ]
-            },
-            raw:true
-        });
+        const pubNotice = await Public.query('SELECT sidoName COUNT(*) FROM pubnotice WHERE sidoName = sidoName OR sidoName LIKE `${spel1}` OR sidoName LIKE `${spel2}` ORDER BY recruitDate (ASC)')
+        // const pubNotice = await Public.findAll({
+        //     order: [["panUploadDate", "ASC"]],
+        //     where: {
+        //         [or]: [
+        //             {
+        //                 sidoName: {
+        //                     [like]: `%${spell1}%`
+        //                 },
+        //                 sidoName:{
+        //                     [like]:`%${spell2}%`
+        //                 }
+        //             }
+        //         ]
+        //     },
+        //     raw:true
+        // });
         //console.log(pubNotice);
         res.send({ result: pubNotice })
     } catch (error) {
@@ -40,11 +41,11 @@ const getPublicNotice = async (req, res) => {
 //         order: [["panUploadDate", "ASC"]],
 //         where :{sidoName :{[like] :`%${sidoName}%`}}
 //     })
-    
+
 //     res.send({ pubNotice2 })
 // }
 
 module.exports = {
     getPublicNotice,
-   // getPublicNotice2
+    // getPublicNotice2
 }
