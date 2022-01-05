7d02dbbc5303ea9effa648697baa1d77e2d24565
hana <aswjdgksk@gmail.con>
2022-01-05 17:42:47 +0900

    dep

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -3,14 +3,12 @@ REPOSITORY=/home/ubuntu/build
 cd $REPOSITORY
 
 # nvm 환경변수 등록
-export NVM_DIR="/home/ubuntu/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+# export NVM_DIR="/home/ubuntu/.nvm"
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
-cd secret
-sudo mv .env.development ../
-cd ..
-
-npm start
+cd server
+sudo npm install
+npm start app.js
 
 
