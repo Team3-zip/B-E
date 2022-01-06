c4817099bda6e02353d1cfb1de8cc927a068a8dc
hana <aswjdgksk@gmail.con>
2022-01-06 23:09:25 +0900

    댓글작성코드수정

--- server/routes/comment.js
+++ server/routes/comment.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middleware/authmiddleware');
 const commentController = require('../controller/comment');
+// const sidoMiddleware = require('../middleware/sidomiddleware')
 
 
-
-router.get('/:aptNo',authMiddleware,commentController.getComment);
+router.get('/:aptNo',commentController.getComment);
 router.post('/:aptNo',authMiddleware, commentController.postComment);
 router.patch('/:aptNo/:commentId', authMiddleware,commentController.patchComment);
 router.delete('/:aptNo/:commentId', authMiddleware,commentController.deleteComment);
