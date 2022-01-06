249ae6cc6bf440c97ba6d92fad150a0c67edc402
hana <aswjdgksk@gmail.con>
2022-01-06 15:04:10 +0900

    deploy

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -7,8 +7,9 @@ cd $REPOSITORY
 # [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 # [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
-cd server
+
 sudo pm2 kill
+cd server
 sudo rm -rf node_modules
 sudo npm install
 sudo pm2 start app.js
