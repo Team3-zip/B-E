adbd3c008be73e6da88a9e4a974f856aa0be071f
hana <aswjdgksk@gmail.con>
2022-01-06 16:25:34 +0900

    controller-user,detail

--- server/config/config.js
+++ server/config/config.js
@@ -11,7 +11,7 @@ const development = {
   //port: env.MYSQL_PORT
 };
 
-const production = {
+const test = {
   username: env.USER_NAME,
   password: env.USER_PASSWORD,
   database: env.DATABASE,
@@ -21,10 +21,10 @@ const production = {
   //port: env.MYSQL_PORT
 };
 
-const test = {
-  username: env.MYSQL_USERNAME,
-  password: env.MYSQL_PASSWORD,
-  database: env.MYSQL_DATABASE_TEST,
+const production = {
+  username: env.MYSQL_USER_NAME,
+  password: env.MYSQL_USER_PASSWORD,
+  database: env.MYSQL_DATABASE,
   host: env.MYSQL_HOST,
   dialect: "mysql",
   timezone:"+09:00"
