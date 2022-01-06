13f5dbdf71906313a786ea9545a84ddfaa751e30
hana <aswjdgksk@gmail.con>
2022-01-06 13:34:27 +0900

    detailcontroller

--- server/routes/detail.js
+++ server/routes/detail.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const detailController = require('../controller/detail');
+const sidoMiddleware = require('../middleware/sidomiddleware')
 
 
-
-router.get('/private/:aptNo',sidomiddleware,detailController.getPrivateDetail);
-router.get('/public/:aptNo', sidomiddleware,detailController.getPublicDetail);
-router.get('/private/:aptNo/img', sidomiddleware,detailController.getPrivateImgUrl);
-router.get('/public/:aptNo/img', sidomiddleware,detailController.getPublicImgUrl);
+router.get('/private/:aptNo',sidoMiddleware,detailController.getPrivateDetail);
+router.get('/public/:aptNo', sidoMiddleware,detailController.getPublicDetail);
+router.get('/private/:aptNo/img', sidoMiddleware,detailController.getPrivateImgUrl);
+router.get('/public/:aptNo/img', sidoMiddleware,detailController.getPublicImgUrl);
 module.exports = router
