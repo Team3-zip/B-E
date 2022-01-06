f5dee2237702d50cc7dc7b4bd3209bd901e17c89
hana <aswjdgksk@gmail.con>
2022-01-06 15:11:20 +0900

    deploy

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 REPOSITORY=/home/ubuntu/build
+
+sudo pm2 kill
 cd $REPOSITORY
 
 # nvm 환경변수 등록
@@ -7,8 +9,6 @@ cd $REPOSITORY
 # [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 # [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
-
-sudo pm2 kill
 cd server
 sudo rm -rf node_modules
 sudo npm install
