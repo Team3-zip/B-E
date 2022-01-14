0241716656609e4f07a3da7225ee17febfb83a40
hana <aswjdgksk@gmail.con>
2022-01-14 22:13:43 +0900

    user이메일수정api

--- server/routes/mypage.js
+++ server/routes/mypage.js
@@ -5,5 +5,6 @@ const router = express.Router()
 
 router.get('/:userKey', authmiddleware, mypageControll.getMypage)
 router.put('/:userName', authmiddleware, mypageControll.putMypage)
+router.put('/:userName/email', authmiddleware, mypageControll.putEmail)
 
 module.exports = router
