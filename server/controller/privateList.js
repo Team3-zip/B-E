0a3333f06ec8afaf15a7ee36bb1a85c282ce5e1b
hana <aswjdgksk@gmail.con>
2022-01-15 11:06:48 +0900

    privateList 상태변경코드수정

--- server/controller/privateList.js
+++ server/controller/privateList.js
@@ -25,17 +25,66 @@ const getPrivateNotice = async (req, res) => {
     let statusDate='';
     let statusArr =[];
     console.log(sDate);
-    statusDate = await Private.findAll({
-        order: [['recruitDate', 'DESC']], //
-        attributes :['recruitDate', 'receptStartDate','receptEndDate', 'pblancNo'],
-        where :{
-            sido:
-            {
-            [like]:"%"+sido+"%"
-            }
-        },
-        raw:true
-    });
+    try{
+        if(spell2 =='상'){
+            statusDate = await Private.findAll({
+                order: [['recruitDate', 'DESC']], //
+                attributes :['recruitDate', 'receptStartDate','receptEndDate', 'pblancNo'],
+                where :{
+                    [or]:[
+                        {
+                            sido:{ [like]:'%경북%'},
+    
+                        },{
+                            sido :{[like] :'%경남%'}
+                        }
+                    ]
+                },
+                  
+                raw:true
+            });
+        }else if(spell2 === '기'){
+            statusDate = await Private.findAll({
+                order: [['recruitDate', 'DESC']], //
+                attributes :['recruitDate', 'receptStartDate','receptEndDate', 'pblancNo'],
+                where :{
+                        sido:{ [like]:'%경기%'},
+                    },
+                raw:true
+            });
+        }else if(spell2 ==='구'){
+            statusDate = await Private.findAll({
+                order: [['recruitDate', 'DESC']], //
+                attributes :['recruitDate', 'receptStartDate','receptEndDate', 'pblancNo'],
+                where :{
+                        sido:{ [like]:'%대구%'},
+                    },
+                raw:true
+            });
+        }else if(spell2 ==='전'){
+            statusDate = await Private.findAll({
+                order: [['recruitDate', 'DESC']], //
+                attributes :['recruitDate', 'receptStartDate','receptEndDate', 'pblancNo'],
+                where :{
+                        sido:{ [like]:'%대전%'},
+                    },
+                raw:true
+            });
+        }else{
+            statusDate = await Private.findAll({
+                order: [['recruitDate', 'DESC']], //
+                attributes :['recruitDate', 'receptStartDate','receptEndDate', 'pblancNo'],
+                where :{
+                        sido:{ [like]:`${spell1}%`},
+                    },
+                raw:true
+            });
+        }
+    }catch(error){
+        console.log('status 값 못 가져옴 ')
+    }
+    
+    
     try {
         if (spell2 === '상') {
             priNotice = await sequelize.query(`SELECT privateapts.*,(SELECT privateAptDetail1.applyAddress FROM privateAptDetail1 WHERE privateapts.pblancNo = privateAptDetail1.fk_pblancNo) as address,
@@ -65,7 +114,7 @@ const getPrivateNotice = async (req, res) => {
             priNotice = await sequelize.query(`SELECT privateapts.*,(SELECT privateAptDetail1.applyAddress FROM privateAptDetail1 WHERE privateapts.pblancNo = privateAptDetail1.fk_pblancNo) as address,
             (SELECT concat(min(cast(privateAptDetail2.supplyAreaSize as decimal(10,4))),'~',max(cast(privateAptDetail2.supplyAreaSize as decimal(10,4)))) as size FROM zip_dev.privateAptDetail2 where privateapts.pblancNo = privateAptDetail2.fk_pblancNo group by fk_pblancNo) as size,
             (SELECT concat(format(min(cast(replace(privateAptDetail2.supplyAmount,",",'') as unsigned)),0),'~', format(max(cast(replace(privateAptDetail2.supplyAmount,",",'')as unsigned)),0)) as supplyAmount FROM zip_dev.privateAptDetail2 where privateapts.pblancNo = privateAptDetail2.fk_pblancNo group by fk_pblancNo) as supplyAmount,
-            (SELECT privateimgs.url1 FROM privateimgs WHERE privateapts.pblancNo = privateimgs.fk_pblancNo) AS ImgUrl , CASE WHEN likes.fk_pblancNo IS NULL THEN 'false' ELSE 'true' END AS islike FROM privateapts LEFT JOIN likes ON privateapts.pblancNo = likes.fk_pblancNo AND likes.fk_userKey='${userKey}' WHERE privateapts.sido LIKE '%${spell1}%' ORDER BY privateapts.recruitDate DESC`)
+            (SELECT privateimgs.url1 FROM privateimgs WHERE privateapts.pblancNo = privateimgs.fk_pblancNo) AS ImgUrl , CASE WHEN likes.fk_pblancNo IS NULL THEN 'false' ELSE 'true' END AS islike FROM privateapts LEFT JOIN likes ON privateapts.pblancNo = likes.fk_pblancNo AND likes.fk_userKey='${userKey}' WHERE privateapts.sido LIKE '${spell1}%' ORDER BY privateapts.recruitDate DESC`)
         }
         for(let i in statusDate){
             const stDate=(statusDate[i]['receptStartDate']).replace(/-/g, '');
